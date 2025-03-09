8ef38a357c41672f992251818b2ac4ce04cd821d
Robert Vo <vohoangquoc992001@gmail.com>
2025-03-09 08:18:38 +0700

    update

--- database.py
+++ database.py
@@ -2,11 +2,19 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql+asyncpg://neondb_owner:npg_LSsi8wP1cUmA@ep-bitter-cloud-a1mre7ly-pooler.ap-southeast-1.aws.neon.tech/neondb"
-# engine = create_async_engine(DATABASE_URL, echo=True, future=True)
-engine = create_async_engine(DATABASE_URL, echo=True, future=True, pool_size=5, max_overflow=10)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True,
+    pool_size=5,
+    max_overflow=10
+)
 
 AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_db():
     async with AsyncSessionLocal() as session:
-        yield session
+        try:
+            yield session
+        finally:
+            await session.close()
